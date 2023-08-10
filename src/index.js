20cabcf2e297dcb8fc2ec8a44bd92d129ca31dd5
xkalepar <132944315+xkalepar@users.noreply.github.com>
2023-08-10 15:24:56 +0200

    build producion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
